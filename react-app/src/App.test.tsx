9a9f5cb0a79f1e25ee2215ac578cda03ef5219a5
Lars Wobus <lars.wobus@web.de>
2020-06-13 10:33:15 +0200

    App renders custom <ul> and <p> content

--- react-app/src/App.test.tsx
+++ react-app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders lorem ipsum', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Lorem Ipsum/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ react-app/src/components/app-wrapper.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const AppWrapper = () => {
+  return <>
+    <div id='nav-bar'>
+      <ul>
+        <li>Button 1</li>
+        <li>Button 2</li>
+        <li>Button 3</li>
+      </ul>
+    </div>
+    <div id='content-wrapper'>
+      <p>Lorem Ipsum Dolor Sit Amet</p>
+    </div>
+  </>
+};
